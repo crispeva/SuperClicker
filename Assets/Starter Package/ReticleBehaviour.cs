8080ba1a045c04e59c27d466eee25775b8560768
crispeva <crisp8dmo@gmail.com>
2025-11-15 11:47:47 +0100

    Curso AR completed

---
+++ Assets/Starter Package/ReticleBehaviour.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ReticleBehaviour : MonoBehaviour
+{
+    public GameObject Child;
+    public DrivingSurfaceManager DrivingSurfaceManager;
+
+    public ARPlane CurrentPlane;
+
+    private Vector3 _screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+   [SerializeField] private GameObject _machineprefab;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Child = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+       List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        DrivingSurfaceManager.RaycastManager.Raycast(_screenCenter,hits,TrackableType.PlaneWithinBounds);
+       CurrentPlane = null;
+        ARRaycastHit? hit;
+        if (hits.Count > 0)
+        {
+            CurrentPlane = DrivingSurfaceManager.LockedPlane;
+            if (CurrentPlane == null)
+            {
+                hit = hits[0];
+
+            }
+            else
+            {
+                hit = hits.SingleOrDefault(x => x.trackableId == CurrentPlane.trackableId);
+
+            }
+
+            if (hit.HasValue)
+            {
+                CurrentPlane = DrivingSurfaceManager.PlaneManager.GetPlane(hit.Value.trackableId);
+                transform.position=hit.Value.pose.position;
+            }
+            if(CurrentPlane != null)
+            {
+                Child.SetActive(true);
+                if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                 GameObject machine = Instantiate(_machineprefab,transform.position + Vector3.up*0.7f, _machineprefab.transform.rotation);
+                    machine.transform.forward = Camera.main.transform.forward;
+                    machine.transform.rotation = Quaternion.Euler(0, machine.transform.rotation.eulerAngles.y, 0);
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Child.SetActive(false);
+            }
+        }
+    }
+}
